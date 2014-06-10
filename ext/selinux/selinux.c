8df2449cd92bb36646174316e88936396efea1b7
Enrico Bacis <enrico.bacis@gmail.com>
2014-06-10 11:35:50 +0200

    add sql function selinux_check_access

--- ext/selinux/selinux.c
+++ ext/selinux/selinux.c
@@ -1,7 +1,8 @@
 /* SeSqlite extension to add SELinux checks in SQLite */
 
-#include <sqlite3ext.h>
 #include <stdlib.h>
+#include <selinux/selinux.h>
+#include <sqlite3ext.h>
 
 SQLITE_EXTENSION_INIT1
 
@@ -20,7 +21,21 @@ int selinux_authorizer(
   return rc;
 }
 
-/* ^ AUTHORIZER ^ */
+static void selinuxCheckAccessFunction(
+  sqlite3_context *context,
+  int argc,
+  sqlite3_value **argv
+){
+   int rc = selinux_check_access(
+     sqlite3_value_text(argv[0]),    /* source security context */
+     sqlite3_value_text(argv[1]),    /* target security context */
+     sqlite3_value_text(argv[2]),    /* target security class string */
+     sqlite3_value_text(argv[3]),    /* requested permissions string */
+     NULL                            /* auxiliary audit data */
+   );
+
+   sqlite3_result_int(context, rc == 0);
+}
 
 int sqlite3_selinux_init(
   sqlite3 *db,
@@ -30,7 +45,12 @@ int sqlite3_selinux_init(
   int rc = SQLITE_OK;
   SQLITE_EXTENSION_INIT2(pApi);
 
+  sqlite3_create_function(db, "selinux_check_access", 4, SQLITE_UTF8 /* | SQLITE_DETERMINISTIC */,
+		                  0, selinuxCheckAccessFunction, 0, 0);
+
   sqlite3_set_authorizer(db, selinux_authorizer, NULL);
+
+  //sqlite3_create_module(db, "");
 
   return rc;
 }
