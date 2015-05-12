3dff496f566500d49f389e989d0d2b1bcc73e9f0
enricobacis <enrico.bacis@gmail.com>
2015-05-12 16:32:13 +0200

    Prepare for different hashmap implementations

--- ext/security/sesqlite/hash/sesqlite_hash_wrapper.c
+++ ext/security/sesqlite/hash/sesqlite_hash_wrapper.c
@@ -1,7 +1,1 @@
-#if !defined(SQLITE_CORE) || defined(SQLITE_ENABLE_SELINUX)
-
-#include "sesqlite_hash_wrapper.h"
-#include "sesqlite_hash_impl.h"
-
-#endif /* !defined(SQLITE_CORE) || defined(SQLITE_ENABLE_SELINUX) */
-
+sqlite/sesqlite_hash_wrapper.c
