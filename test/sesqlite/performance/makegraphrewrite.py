b2d5583c653d6d498dbe0e17410defdd0d05e31a
enricobacis <enrico.bacis@gmail.com>
2015-06-08 10:16:40 +0200

    Final touches to makegraphrewrite

--- test/sesqlite/performance/makegraphrewrite.py
+++ test/sesqlite/performance/makegraphrewrite.py
@@ -20,6 +20,7 @@ if __name__ == '__main__':
     ys_over_sca = 100 * (ys_se_sca - ys_base) / ys_se_sca
     ys_over_in = 100 * (ys_se_in - ys_base) / ys_se_in
     ys_over_min = np.minimum(ys_over_sca, ys_over_in)
+    top = max(np.max(ys_over_in), np.max(ys_over_sca))
 
     cross = max(xs)
     for i in xrange(len(xs)):
@@ -30,14 +31,20 @@ if __name__ == '__main__':
     plt.plot(xs, ys_over_sca, color=cmap(.4), linewidth=2, marker='^', label='selinux_check_access')
     plt.plot(xs, ys_over_in, color=cmap(.8), linewidth=2, marker='o', label='SQL IN clause')
     plt.plot(xs, ys_over_min, color= 'r', linewidth=4, linestyle='--', label='SeSQLite')
-    p = plt.axvspan(1, cross, color=cmap(.8), alpha=0.2)
-    p = plt.axvspan(cross, max(xs), color=cmap(.4), alpha=0.2)
-    plt.ylim(bottom=0)
+    plt.axvspan(1, cross, color=cmap(.8), alpha=0.2)
+    plt.axvspan(cross, max(xs), color=cmap(.4), alpha=0.2)
     plt.xlim(1, max(xs))
+    plt.ylim(bottom=0, top=top+2.5)
     plt.legend(loc='lower right')
     plt.ylabel('overhead')
     plt.xlabel('tuple contexts')
     plt.gca().yaxis.set_major_formatter(mtick.FormatStrFormatter('%.0f%%'))
+
+    ary = top + 2
+    plt.annotate('', xy=(1, ary), xytext=(cross, ary), arrowprops={'arrowstyle':'->'})
+    plt.annotate('', xy=(max(xs), ary), xytext=(cross, ary), arrowprops={'arrowstyle':'->'})
+    plt.text(.5*(1+cross), ary -1, 'SQL IN', va='bottom', ha='center')
+    plt.text(.5*(cross+max(xs)), ary -1, 'selinux_check_access', va='bottom', ha='center')
     plt.tight_layout()
     plt.savefig('results/rewrite/rewrite.pdf')
 
